492257eae5abff1608998644637e8666d973a31f
Josh Lemmond <joshualemmond@gmail.com>
2023-01-23 12:08:38 -0500

    fixed a bug with products routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,6 +42,16 @@ router.get('/:id', (req, res) => {
       }
     ]
   })
+  .then(data => {
+    if(!data) {
+      res.status(404).json({ message: 'No product with this ID found'});
+      return;
+    };
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -119,7 +129,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(data => {
+    if (!data) {
+      res.status(404).json({ message: 'No product with this ID found' });
+      return;
+    }
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
